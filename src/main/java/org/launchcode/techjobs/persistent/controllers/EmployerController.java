ec60f960c7851c8360547c70fce9a947679a1c5d
Jerihmy <jhickso1@student.ccp.edu>
2022-10-09 05:26:44 -0400

    issue with save method for Employer repository

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -14,19 +16,37 @@ import java.util.Optional;
 public class EmployerController {
 
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+
+    //Add an index method that responds at /employers with a list of all employers in the database. This method should use the template --> employers/index
+    @PostMapping("")
+public String index(Model model){
+    model.addAttribute("title", "All Employers");
+    model.addAttribute("employers", employerRepository.findAll());
+    return "employers/index";
+}
+
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
         model.addAttribute(new Employer());
         return "employers/add";
     }
 
+
+    // Use employerRepository and the appropriate method to save a valid object...
     @PostMapping("add")
     public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Employer");
+            model.addAttribute("employers", employerRepository.findAll());
             return "employers/add";
         }
+        //add save method here for object...
+        //employerRepository.save(newEmployer);
 
         return "redirect:";
     }
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotEmpty @NotNull @Size(min = 0, max = 70)
+    private String location;
 
+    public Employer () {};
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
