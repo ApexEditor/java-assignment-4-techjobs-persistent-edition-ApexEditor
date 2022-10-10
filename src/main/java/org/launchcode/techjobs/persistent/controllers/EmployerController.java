dcc900e4b7578c6d17c836ba95b34adb57377321
Jerihmy <jhickso1@student.ccp.edu>
2022-10-09 21:40:49 -0400

    Fixed both save method issues

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,7 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,7 +24,6 @@ public class EmployerController {
     //Add an index method that responds at /employers with a list of all employers in the database. This method should use the template --> employers/index
     @PostMapping("")
 public String index(Model model){
-    model.addAttribute("title", "All Employers");
     model.addAttribute("employers", employerRepository.findAll());
     return "employers/index";
 }
@@ -35,18 +35,20 @@ public String index(Model model){
     }
 
 
-    // Use employerRepository and the appropriate method to save a valid object...
+    // Controllers part 3. Use employerRepository and the appropriate method to save a valid object...
     @PostMapping("add")
     public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
-            model.addAttribute("title", "Add Employer");
-            model.addAttribute("employers", employerRepository.findAll());
+//            model.addAttribute("title", "Add Employer");
+//            model.addAttribute("employer", employerRepository);
             return "employers/add";
         }
         //add save method here for object...
-        //employerRepository.save(newEmployer);
+
+        employerRepository.save(newEmployer);
+        //model.addAttribute("employer", employerRepository.findAll());
 
         return "redirect:";
     }
@@ -54,7 +56,10 @@ public String index(Model model){
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+
+        //Replace -->null<-- with .findById() method with the right argument to look for the given employer object from the data layer.
+
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,15 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @NotEmpty @NotNull @Size(min = 0, max = 70)
     private String location;
+
+    @OneToMany @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer () {};
 
@@ -19,5 +28,8 @@ public class Employer extends AbstractEntity {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public void save(EmployerRepository employerRepository) {
     }
 }
